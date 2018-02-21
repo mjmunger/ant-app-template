b34c686071dd667af1ba710b1dc34d5f16c54f24
Michael Munger <michael@highpoweredhelp.com>
2018-02-21 08:04:17 -0500

    Merged autoloader into default template.

--- ant-app-template.php
+++ ant-app-template.php
@@ -106,7 +106,44 @@ class ${SYSTEMNAME} extends AntApp  implements AppInterface  {
         return $results;
     }
 
-${AUTOLOADER}
+    function ${SYSTEMNAME}AutoLoader() {
+        //REGISTER THE AUTOLOADER! This has to be done first thing! 
+        spl_autoload_register(array($this,'load${SYSTEMNAME}Classes'));
+        return ['success' => true];
+    }
+
+    public function load${SYSTEMNAME}Classes($class) {
+        
+        //Break apart namespaced requests.
+
+        $buffer = explode("\\",$class);
+        $class = end($buffer);
+
+        $baseDir = $this->path;
+
+        $candidate_files = array();
+
+        //Try to grab it from the classes directory.
+        $candidate_path = sprintf($baseDir.'/classes/%s.class.php',$class);
+        array_push($candidate_files, $candidate_path);
+
+        //Loop through all candidate files, and attempt to load them all in the correct order (FIFO)
+        foreach($candidate_files as $dependency) {
+            if($this->verbosity > 14) printf("Looking to load %s",$dependency) . PHP_EOL;
+
+            if(file_exists($dependency)) {
+                if(is_readable($dependency)) {
+
+                    //Print debug info if verbosity is greater than 9
+                    if($this->verbosity > 11) print "Including: " . $dependency . PHP_EOL;
+
+                    //Include the file!
+                    include($dependency);
+                }
+            }
+        }
+        return ['success' => true];
+    }
     
     /**
      * Callback function that prints to the CLI during cli-init to show this plugin has loaded.
